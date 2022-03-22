74310ecfbebd31d11571a98adc46352c36659e66
Dhruvik <dhruviksparikh@gmail.com>
2022-03-22 07:07:12 +0000

    Added changes

---
+++ src/test/java/com/simplilearn/MPhasis_Feb_Rest/MPhasisFebRestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.simplilearn.MPhasis_Feb_Rest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MPhasisFebRestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
